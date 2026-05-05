d03b6a00a80bcccc52ce2f4b8ed4b1ec58aaf4a4
Alexander Gerhard <870717ag@gmail.com>
2026-05-05 07:38:40 +0200

    fix: correct base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+function getBase() {
+  return process.env.NODE_ENV === 'production' ? '/' : '/'
+}
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-3/',
+  base: getBase(),
 })
